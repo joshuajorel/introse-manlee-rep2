79bceb226a1f789d329c76b2aee17ef7c8c5cca6
justinmandac <justinmandac@gmail.com>
2012-07-31 16:46:54 +0800

    Employee Class

---
+++ Objects/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace introseHHC.Objects
+{
+    public class Employee:Person
+    {
+         public Employee()
+        {
+            fname = sname = mname = "";
+            gender = civstat = "";
+            nationality = educattain = email = "";
+            age = 0;
+
+        }
+    }
+}
