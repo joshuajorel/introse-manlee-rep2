cf8686330a161d93e52e88326f40aebcfc477e93
jorel000 <joshua.jorel.lee@gmail.com>
2012-08-01 03:02:06 +0800

    updated from last push of master branch

--- RegForms/MainMenu.Designer.cs
+++ RegForms/MainMenu.Designer.cs
@@ -78,7 +78,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(263, 216);
+            this.ClientSize = new System.Drawing.Size(263, 259);
             this.Controls.Add(this.regEmpButton);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.manageRecButton);
